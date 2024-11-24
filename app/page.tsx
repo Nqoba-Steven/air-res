2c18bfd975c96026655153a2c923ca0dab5df5c1
Nqoba-Steven <nqoba.novukuza@gmail.com>
2024-11-24 16:40:39 +0200

    Jenkins Docker Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import nex from "../public/next.svg"
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src={nex}
           alt="Next.js logo"
           width={180}
           height={38}
@@ -32,7 +33,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="/public/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -58,7 +59,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="/public/file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -73,7 +74,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="/public/window.svg"
             alt="Window icon"
             width={16}
             height={16}
